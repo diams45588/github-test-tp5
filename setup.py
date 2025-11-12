0296791a5cb915b4d70cf18dd6f4b1bac077874c
diams45588 <diaminatousanogo4@gmail.com>
2025-11-12 22:24:14 +0000

    Application Flask Fl

---
+++ setup.py
@@ -0,0 +1,10 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="flask-app-tp5",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "Flask==2.3.3",
+    ],
+)
